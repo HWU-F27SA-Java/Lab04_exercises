5968c9fc1522188a369144e655a6a09b39717b8c
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:27:56 +0100

    Create Moor.java

---
+++ src/Moor.java
@@ -0,0 +1,22 @@
+import java.text.DateFormatSymbols;
+import java.util.Scanner;
+
+public class Moor {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a month (1 to 12) ");
+		int m = input.nextInt();
+		System.out.print("Enter a year posterior to 2006 ");
+		int y = input.nextInt();
+
+
+		input.close();
+	}
+
+	public static double moor(int month, int year) {
+		double performance =0;
+		
+		return performance;
+	}
+}
