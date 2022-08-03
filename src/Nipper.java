4d7a27f589fa8965c385e4168f7e7f6b2bd6b8bb
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:27:58 +0100

    Create Nipper.java

---
+++ src/Nipper.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Nipper {
+
+	public static void main(String[] args) {
+
+
+	}
+	public static long factorial(int n) {
+		if(n<=1) return 1;
+		return n*factorial(n-1);
+	}
+	public static double niper (double threshold) {
+		double e = 0;
+		
+		
+		return e;
+	}
+
+}
