ddd21821326f550572a3dfc0b1a8602a510080b3
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:28:01 +0100

    Create StoppingDistance.java

---
+++ src/StoppingDistance.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StoppingDistance {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static float getStoppingDistance(int s, float f, float h) {
+		float ds = 0;
+
+		return ds;
+	}
+}
